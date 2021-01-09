e25169b845b5f45f6801b942108bcf95a375b53b
Andy Kwong <yantkwong@gmail.com>
2021-01-08 16:23:56 -0800

    basic setup

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+import pprint
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_hi() -> None:
+    url = "https://google.com"
+    page = requests.get(url)
+    print(page.content)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

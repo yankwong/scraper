001c04cfa8a415729640a4e612216b9f93fd5fe5
Andy Kwong <yantkwong@gmail.com>
2021-01-09 14:50:30 -0800

    added card type parsing

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ three_star_total = parser.get_total_rarity(fgo_na_tier_list, 3)
 four_star_total = parser.get_total_rarity(fgo_na_tier_list, 4)
 five_star_total = parser.get_total_rarity(fgo_na_tier_list, 5)
 
+total_support = parser.get_servant_type(fgo_na_tier_list, 'SUPPORT')
+total_aoe = parser.get_servant_type(fgo_na_tier_list, 'AOE')
+total_st = parser.get_servant_type(fgo_na_tier_list, 'ST')
+
 total_servants = one_star_total + two_star_total + three_star_total + four_star_total + five_star_total
 
 if __name__ == '__main__':
@@ -33,3 +37,8 @@ if __name__ == '__main__':
     print(f'{three_star_total} of them are three stars')
     print(f'{four_star_total} of them are four stars')
     print(f'{five_star_total} of them are five stars')
+    print('-------------------')
+    print(f'{total_st} of them are SINGLE TARGET servant')
+    print(f'{total_aoe} of them are AOE servant')
+    print(f'{total_support} of them are SUPPORT servant')
+
--- parser.py
+++ parser.py
@@ -9,16 +9,26 @@ rarity_translator = {
     5: '★★★★★',
 }
 
+
 def get_page_json(page_content: str):
     return BeautifulSoup(page_content, 'html.parser')
 
 
 def get_total_rarity(tier_list_json, target_rarity) -> int:
-    all_rarities = tier_list_json.find_all('span', class_='star-rarity')
     total = 0
+    all_rarities = tier_list_json.find_all('span', class_='star-rarity')
 
     for rarity in all_rarities:
         if rarity.text == rarity_translator[target_rarity]:
             total += 1
+    return total
 
+
+def get_servant_type(tier_list_json, servant_type) -> int:
+    total = 0
+    all_servant_types = tier_list_json.select('.FGOTierNPInfo > div')
+
+    for s_type in all_servant_types:
+        if s_type.text == servant_type:
+            total += 1
     return total
